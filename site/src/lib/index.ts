e3531cc6285d517420ec699d019364bdbf66685b
K <25322315+boredcodebyk@users.noreply.github.com>
2025-04-17 13:01:34 +0530

    init

---
+++ site/src/lib/index.ts
@@ -0,0 +1,15 @@
+// place files you want to import through the `$lib` alias in this folder.
+
+interface Link {
+    id: string,
+    name: string,
+    dateCreated: Date,
+    link: string
+}
+
+const db: Link[] = [
+
+]
+
+
+export {db};
