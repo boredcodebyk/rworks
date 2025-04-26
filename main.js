d37e15ed8c03ad9e2510bcd7d74bcbcfa4754a92
K <25322315+boredcodebyk@users.noreply.github.com>
2025-04-26 16:00:33 +0530

    update main.js

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const db = [
         id: "0bf6acc2-8822-4f5b-8f42-c5a9e9b5612b",
         name: "TidyTuesday Week 16",
         date: new Date(1745662110488),
-        link: "shiny/tidytuesday/2025/16/"
+        link: "code/tidytuesday/2025/16/"
     },
     {
         id: "948ce290-4167-43ff-ae8b-4ee043158fd2",
@@ -16,18 +16,19 @@ const db = [
 ]
 
 function init() {
-    let item = document.createElement("p");
+    let item = document.createElement("tbody");
     if (db.length != 0) {
-
         for (const i in db) {
+            let tr = document.createElement("tr");
             const element = db[i];
             let link = document.createElement("a");
             link.innerHTML = `<a href="${element.link}">${element.name}</a>`;
             let date = moment(element.date).format("MMMM Do YYYY");
-            item.append(`[${date}]`," | ", link);
+            tr.innerHTML = `<td>[${date}]</td><td>|</td><td><a href="${element.link}">${element.name}</a></td>`;
+            item.append(tr)
         }
     } else {
-        item.innerHTML = "Come back later";
+        item.innerHTML = "<tr>Come back later<tr>";
     }
 
     document.querySelector("#list").append(item);
